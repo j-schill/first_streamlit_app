59602d6de8e3f45f34f3a2a40b93d6944a83cc40
Joshua Schill <69824228+J-SCHILL@users.noreply.github.com>
2023-09-12 15:05:25 -0500

    Update streamlit_app.py
    clean up

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,6 @@ import pandas as pd
 sl.title('My Parents New Healthy Dinner')
 
 sl.header('Breakfast Menu')
-
 sl.text('Omega 3 & Blueberry Oatmeal')
 sl.text('Kale, Spinach & Rocket Smoothie')
 sl.text('Hard-Boiled Free-Range Egg')
-
-
-sl.text('updated! :)')
