fe0c0dbb269ca71f403392250d310ec810128f0f
Joshua Schill <69824228+J-SCHILL@users.noreply.github.com>
2023-09-12 15:07:03 -0500

    Update streamlit_app.py
    added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import pandas as pd
 sl.title('My Parents New Healthy Dinner')
 
 sl.header('Breakfast Menu')
-sl.text('Omega 3 & Blueberry Oatmeal')
-sl.text('Kale, Spinach & Rocket Smoothie')
-sl.text('Hard-Boiled Free-Range Egg')
+sl.text('🥣 Omega 3 & Blueberry Oatmeal')
+sl.text('🥗 Kale, Spinach & Rocket Smoothie')
+sl.text('🐔 Hard-Boiled Free-Range Egg')
+sl.text('🥑🍞 Avocado Toast')
