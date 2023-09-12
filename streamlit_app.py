bde1b12ba17f8e66f59cf9dc0741614f2abf7e77
Joshua Schill <69824228+J-SCHILL@users.noreply.github.com>
2023-09-12 12:04:26 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as sl
+import pandas as pd
 
 sl.title('My Parents New Healthy Dinner')
 
@@ -7,3 +8,10 @@ sl.header('Breakfast Menu')
 sl.text('Omega 3 & Blueberry Oatmeal')
 sl.text('Kale, Spinach & Rocket Smoothie')
 sl.text('Hard-Boiled Free-Range Egg')
+
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
