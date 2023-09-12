c4e80fde1d516a354099fb25424506422294e02e
Joshua Schill <69824228+J-SCHILL@users.noreply.github.com>
2023-09-12 13:34:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,12 +8,3 @@ sl.header('Breakfast Menu')
 sl.text('Omega 3 & Blueberry Oatmeal')
 sl.text('Kale, Spinach & Rocket Smoothie')
 sl.text('Hard-Boiled Free-Range Egg')
-
-
-st.text('Heres our first attempt at using data to create a table:')
-st.table(
-    pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-})
-)
