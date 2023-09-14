a31ad1a0a31f2467bebe4e8a3ded3e07819565b2
Joshua Schill <69824228+J-SCHILL@users.noreply.github.com>
2023-09-14 15:29:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,3 +28,10 @@ sl.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 sl.text(fruityvice_response.json())
+
+
+
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+sl.dataframe(fruityvice_normalized)
